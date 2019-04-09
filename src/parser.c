228697317ca52f55dd2d2abdffe6c6f9f5e421dc
Abdul-rahman Mohamed <amohamed@e1z2r3p2.42.us.org>
2019-04-08 21:40:11 -0700

    parsing len

--- src/parser.c
+++ src/parser.c
@@ -1,19 +1,38 @@
 #include "ft_printf.h"
-void parse_specifier(va_list list, const char *format, t_block *block)
+void parse_specifier(va_list list, const char **format, t_block *block)
 {
 	const char *key = "cspdiouxXf%";
 	char *pointer_to_spec;
 
-	pointer_to_spec = ft_strchr(key, *format);
+	pointer_to_spec = ft_strchr(key, **format);
 	if (pointer_to_spec)
 	{
 		block->specifier = pointer_to_spec - key;
-		// block->specifier = *format;
 		block->valid = 1;
-		// *format++;
+		(*format)++;
 	}
 	(void)(list);
 	// if (block->valid)
 	// 	call_handler(list, block);
+}
 
+void parse_len(const char **format, t_block *block)
+{
+	if (ft_strnstr(*format, "hh", 2))
+		block->len = hh;
+	else if (**format == 'h')
+		block->len = h;
+	else if (ft_strnstr(*format, "ll", 2))
+		block->len = ll;
+	else if (**format == 'l')
+		block->len = l;
+	else if (**format == 'L')
+		block->len = L;
+	if (block->len)
+	{
+		if (block->len == hh || block->len == ll)
+			(*format) += 2;
+		else
+			(*format) += 1;
+	}
 }
